fae76bcd60a33280a543ddb9733115f5f795adf1
Anna <anna.klimas@gmail.com>
2015-10-27 13:07:35 +0000

    List of lifts displayed in Personal Best tab.

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -2,7 +2,9 @@ angular.module('crossFitApp.controllers', [])
 
 .controller('WODCtrl', function($scope) {})
 
-.controller('PersonalBestCtrl', function($scope) {})
+.controller('PersonalBestCtrl', function($scope, PersonalBest) {
+  $scope.lifts = PersonalBest.all();
+})
 
 .controller('TrainingNotesCtrl', function($scope) {})
 
--- www/js/services.js
+++ www/js/services.js
@@ -1,2 +1,66 @@
 angular.module('crossFitApp.services', [])
 
+.factory('PersonalBest', function() {
+
+  var lifts = [{
+    id: 0,
+    name: 'Back Squat'
+  },
+  {
+    id: 1,
+    name: 'Front Squat'
+  },
+  {
+    id: 2,
+    name: 'Deadlift'
+  },
+  {
+    id: 3,
+    name: 'Power Clean'
+  },
+  {
+    id: 4,
+    name: 'Power Snatch'
+  },
+  {
+    id: 5,
+    name: 'Snatch'
+  },
+  {
+    id: 6,
+    name: 'Thruster'
+  },
+  {
+    id: 7,
+    name: 'Overhead Squat'
+  },
+  {
+    id: 8,
+    name: 'Shoulder/strict press'
+  },
+  {
+    id: 9,
+    name: 'Push Press'
+  },
+  {
+    id: 10,
+    name: 'Push Jerk'
+  },
+  {
+    id: 10,
+    name: 'Clean & Jerk'
+  },
+  {
+    id: 10,
+    name: 'Bench Press'
+  }
+  ];
+
+
+return {
+  all: function() {
+    return lifts;
+  }
+};
+
+});
